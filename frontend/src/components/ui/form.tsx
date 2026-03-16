ba598c42f038a10b7d92cdd0a31c446c2fc0574d
occupy5 <1370275510@qq.com>
2026-03-16 20:52:55 +0800

    feat: update components

---
+++ frontend/src/components/ui/form.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+function Form({ className, ...props }: React.ComponentProps<"form">) {
+  return (
+    <form
+      data-slot="form"
+      className={cn(className)}
+      {...props}
+    />
+  )
+}
+
+export { Form }
